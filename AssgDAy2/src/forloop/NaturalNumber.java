439972020db428c84b01e0c2ca01d2c0ea38173d
Diksha Mendhe <diksshamenddhe97@gmail.com>
2023-06-18 23:33:39 -0700

    Day 2 assinment complete.

---
+++ AssgDAy2/src/forloop/NaturalNumber.java
@@ -0,0 +1,12 @@
+package forloop;
+
+public class NaturalNumber {
+    public static void main(String[] args) {
+        int sum=0;
+        int a=3;
+        for(int i=1;i<=a;i++){
+            sum=sum+i;
+        }
+        System.out.println("Natural number "+sum);
+    }
+}
